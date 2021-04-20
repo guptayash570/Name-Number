d0eb43feec4ec10aa3527b8e27ebc95b90cf0f0a
guptayash570 <57900351+guptayash570@users.noreply.github.com>
2021-04-20 12:05:07 +0530

    Update Options

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const input = document.getElementById('input');
 const output = document.getElementById('out');
-
 var sum = 0;
 function calculate() {
     var masterstring = ' abcdefghijklmnopqrstuvwxyz';
+    const opt = document.getElementById('option').value;
     var txt = input.value;
     var lowtxt = txt.toLowerCase();
 
+    if ( opt == 'a1'){
     for (var i = 0; i < lowtxt.length ; i++){
         sum += masterstring.indexOf(lowtxt.charAt(i));
+        }
+    }
+    else if ( opt == 'a0'){
+        for (var i = 0; i < lowtxt.length ; i++){
+            sum += masterstring.indexOf(lowtxt.charAt(i)) -1 ;
+            }
+        
     }
     output.innerHTML = "Sum = " + sum;
-    console.log(sum);
     sum = 0;
-    console.log(lowtxt);
+    
 }
